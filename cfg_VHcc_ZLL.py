f1025b7745bfb70b255c0a566054ed7f5a4dd5c3
ValVau <109755950+ValVau@users.noreply.github.com>
2024-05-22 14:26:59 +0200

    Adding new variables to the Workflow and saving the arrays

--- cfg_VHcc_ZLL.py
+++ cfg_VHcc_ZLL.py
@@ -48,6 +48,7 @@ files_Run3 = [
 ]
 
 parameters["proc_type"] = "ZLL"
+#parameters["run_period"] = "Run2" # Run2 Or Run3
 
 cfg = Configurator(
     parameters = parameters,
@@ -59,7 +60,7 @@ cfg = Configurator(
             "samples": [
                 "DATA_DoubleMuon",
                 "DATA_DoubleEG", # in 2016/2017
-                "DATA_EGamma",   # in 2018/2022/2023
+                #"DATA_EGamma",   # in 2018/2022/2023
 	        "WW", "WZ", "ZZ",
                 "DYJetsToLL_FxFx",
                 "DYJetsToLL_MLM",
@@ -72,7 +73,6 @@ cfg = Configurator(
             "year": ['2017']
             #"year": ['2016_PreVFP', '2016_PostVFP','2017','2018']
             #"year": ['2022_preEE','2022_postEE','2023_preBPix','2023_postBPix']
-            #"year": ['2022_preEE']
         }
     },
 
@@ -99,7 +99,7 @@ cfg = Configurator(
         "common": {
             "inclusive": ["signOf_genWeight","lumi","XS",
                           "pileup", #Not in 2022/2023
-                          "sf_mu_id","sf_mu_iso",
+                          #"sf_mu_id","sf_mu_iso",
                           "sf_ele_reco","sf_ele_id",
                           #"sf_ctag", "sf_ctag_calib"
                           ],
@@ -116,7 +116,7 @@ cfg = Configurator(
             "common": {
                 "inclusive": [
                     "pileup",
-                    "sf_mu_id", "sf_mu_iso",
+                    #"sf_mu_id", "sf_mu_iso",
                     "sf_ele_reco", "sf_ele_id",
                     #"sf_ctag"
                 ]
@@ -140,15 +140,40 @@ cfg = Configurator(
 	**jet_hists(coll="JetsCvsL", pos=1),
 
         "nJet": HistConf( [Axis(field="nJet", bins=15, start=0, stop=15, label=r"nJet direct from NanoAOD")] ),
-
+        "dilepton_m_low": HistConf( [Axis(field="dilep_mass_low", bins=100, start=0, stop=200, label=r"$M_{\ell\ell}$ [GeV], 50 < $p_{T_{\ell\ell}}$ < 150")] ),
+        "dilepton_pt_low": HistConf( [Axis(field="dilep_pt_low", bins=100, start=0, stop=200, label=r"$p_{T_{\ell\ell}}$ [GeV], 50 < $p_{T_{\ell\ell}}$ < 150")] ),
+        "dilepton_dphi_low": HistConf( [Axis(field="dilep_phi_low", bins=64, start=-1, stop=3.5, label=r"$\Delta \phi_{\ell\ell}$, 50 < $p_{T_{\ell\ell}}$ < 150")] ),
+        "dilepton_deta_low": HistConf( [Axis(field="dilep_eta_low", bins=100, start=-1, stop=4, label=r"$\Delta \eta_{\ell\ell}$, 50 < $p_{T_{\ell\ell}}$ < 150")] ),
+        "dilepton_dijet_ratio_low": HistConf( [Axis(field="pt_ratio_low", bins=100, start=0, stop=2, label=r"$\frac{p_T(\ell\ell)}{p_T(jj)}$, 50 < $p_{T_{\ell\ell}}$ < 150")] ),
+        "dilepton_dijet_dphi_low": HistConf( [Axis(field="ZH_delphi_low", bins=100, start=-4, stop=4, label=r"$\Delta \phi (\ell\ell, jj)$, 50 < $p_{T_{\ell\ell}}$ < 150")] ),
+        "dilepton_l2j1_low": HistConf( [Axis(field="deltaPhi_l2_j1_low", bins=100, start=-1, stop=3.5, label=r"$\Delta \phi (\ell_2, j_1)$, 50 < $p_{T_{\ell\ell}}$ < 150")] ),
+        "dilepton_l2j2_low": HistConf( [Axis(field="deltaPhi_l2_j2_low", bins=100, start=-1, stop=3.5, label=r"$\Delta \phi (\ell_2, j_2)$, 50 < $p_{T_{\ell\ell}}$ < 150")] ),
+        
+        "dilepton_m_high": HistConf( [Axis(field="dilep_mass_high", bins=100, start=0, stop=200, label=r"$M_{\ell\ell}$ [GeV], 150 < $p_{T_{\ell\ell}}$")] ),
+        "dilepton_pt_high": HistConf( [Axis(field="dilep_pt_high", bins=100, start=0, stop=200, label=r"$p_{T_{\ell\ell}}$ [GeV], 150 < $p_{T_{\ell\ell}}$")] ),
+        "dilepton_dphi_high": HistConf( [Axis(field="dilep_phi_high", bins=64, start=-1, stop=3.5, label=r"$\Delta \phi_{\ell\ell}$, 150 < $p_{T_{\ell\ell}}$")] ),
+        "dilepton_deta_high": HistConf( [Axis(field="dilep_eta_high", bins=100, start=-1, stop=4, label=r"$\Delta \eta_{\ell\ell}$, 150 < $p_{T_{\ell\ell}}$")] ),
+        "dilepton_dijet_ratio_high": HistConf( [Axis(field="pt_ratio_high", bins=100, start=0, stop=2, label=r"$\frac{p_T(\ell\ell)}{p_T(jj)}$, 150 < $p_{T_{\ell\ell}}$")] ),
+        "dilepton_dijet_dphi_high": HistConf( [Axis(field="ZH_delphi_high", bins=100, start=-4, stop=4, label=r"$\Delta \phi (\ell\ell, jj)$, 150 < $p_{T_{\ell\ell}}$")] ),
+        "dilepton_l2j1_high": HistConf( [Axis(field="deltaPhi_l2_j1_high", bins=100, start=-1, stop=3.5, label=r"$\Delta \phi (\ell_2, j_1)$, 150 < $p_{T_{\ell\ell}}$")] ),
+        "dilepton_l2j2_high": HistConf( [Axis(field="deltaPhi_l2_j2_high", bins=100, start=-1, stop=3.5, label=r"$\Delta \phi (\ell_2, j_2)$, 150 < $p_{T_{\ell\ell}}$")] ),
+        
         "dilep_m" : HistConf( [Axis(coll="ll", field="mass", bins=100, start=0, stop=200, label=r"$M_{\ell\ell}$ [GeV]")] ),
         "dilep_m_zoom" : HistConf( [Axis(coll="ll", field="mass", bins=40, start=70, stop=110, label=r"$M_{\ell\ell}$ [GeV]")] ),
         "dilep_dr" : HistConf( [Axis(coll="ll", field="deltaR", bins=50, start=0, stop=5, label=r"$\Delta R_{\ell\ell}$")] ),
         "dilep_pt" : HistConf( [Axis(coll="ll", field="pt", bins=100, start=0, stop=400, label=r"$p_T{\ell\ell}$ [GeV]")] ),
 
-        "dijet_m" : HistConf( [Axis(coll="dijet", field="mass", bins=100, start=0, stop=600, label=r"$M_{jj}$ [GeV]")] ),
-        "dijet_dr" : HistConf( [Axis(coll="dijet", field="deltaR", bins=50, start=0, stop=5, label=r"$\Delta R_{jj}$")] ),
-        "dijet_pt" : HistConf( [Axis(coll="dijet", field="pt", bins=100, start=0, stop=400, label=r"$p_T{jj}$ [GeV]")] ),
+        "dijet_m_low" : HistConf( [Axis(field="dijet_m_low", bins=100, start=0, stop=600, label=r"$M_{jj}$ [GeV], 50 < $p_{T_{\ell\ell}}$ < 150")] ),
+        "dijet_dr_low" : HistConf( [Axis(field="dijet_deltaR_low", bins=50, start=0, stop=5, label=r"$\Delta R_{jj}$, 50 < $p_{T_{\ell\ell}}$ < 150")] ),
+        "dijet_dphi_low": HistConf( [Axis(field="dijet_deltaPhi_low", bins=64, start=-1, stop=3.5, label=r"$\Delta \phi_{jj}$, 50 < $p_{T_{\ell\ell}}$ < 150")] ),
+        "dijet_deta_low": HistConf( [Axis(field="dijet_deltaEta_low", bins=100, start=-1, stop=4, label=r"$\Delta \eta_{jj}$, 50 < $p_{T_{\ell\ell}}$ < 150")] ),
+        "dijet_pt_low" : HistConf( [Axis(field="dijet_pt_low", bins=100, start=0, stop=400, label=r"$p_T{jj}$ [GeV], 50 < $p_{T_{\ell\ell}}$ < 150")] ),
+        
+        "dijet_m_high" : HistConf( [Axis(field="dijet_m_high", bins=100, start=0, stop=600, label=r"$M_{jj}$ [GeV], 150 < $p_{T_{\ell\ell}}$")] ),
+        "dijet_dr_high" : HistConf( [Axis(field="dijet_deltaR_high", bins=50, start=0, stop=5, label=r"$\Delta R_{jj}$, 150 < $p_{T_{\ell\ell}}$")] ),
+        "dijet_dphi_high": HistConf( [Axis(field="dijet_deltaPhi_high", bins=64, start=-1, stop=3.5, label=r"$\Delta \phi_{jj}$, 150 < $p_{T_{\ell\ell}}$")] ),
+        "dijet_deta_high": HistConf( [Axis(field="dijet_deltaEta_high", bins=100, start=-1, stop=4, label=r"$\Delta \eta_{jj}$, 150 < $p_{T_{\ell\ell}}$")] ),
+        "dijet_pt_high" : HistConf( [Axis(field="dijet_pt_high", bins=100, start=0, stop=400, label=r"$p_T{jj}$ [GeV], 150 < $p_{T_{\ell\ell}}$")] ),
 
         "dijet_csort_m" : HistConf( [Axis(coll="dijet_csort", field="mass", bins=100, start=0, stop=600, label=r"$M_{jj}$ [GeV]")] ),
         "dijet_csort_dr" : HistConf( [Axis(coll="dijet_csort", field="deltaR", bins=50, start=0, stop=5, label=r"$\Delta R_{jj}$")] ),
@@ -166,5 +191,30 @@ cfg = Configurator(
                                    bins=[0,80,150,200,300,450,700],
                                    type="variable",
                                    label="Jets $H_T$ [GeV]")]),
+        "dphi_jj_dr_jj": HistConf([ Axis(field="dijet_deltaR_low", bins=50, start=0, stop=5, label=r"$\Delta R_{jj}$, 50 < $p_{T_{\ell\ell}}$ < 150"),
+                              Axis(field="dijet_deltaPhi_low", bins=64, start=-1, stop=3.5, label=r"$\Delta \phi_{jj}$, 50 < $p_{T_{\ell\ell}}$ < 150")]),
     }
 )
+
+
+run_options = {
+    "executor"       : "parsl/condor",
+    "env"            : "conda",
+    "workers"        : 1,
+    "scaleout"       : 10,
+    "walltime"       : "00:60:00",
+#    "mem_per_worker_parsl" : 2, # For Parsl
+    "mem_per_worker" : "2GB", # For Dask
+    "exclusive"      : False,
+    "skipbadfiles"   : False,
+    "chunk"          : 500000,
+    "retries"        : 10,
+    "treereduction"  : 20,
+    "adapt"          : False,
+    "requirements": (
+        '( TotalCpus >= 8) &&'
+        '( Machine != "lx3a44.physik.rwth-aachen.de" ) && ' 
+        '( Machine != "lx3b80.physik.rwth-aachen.de" )'
+    ),
+    
+}
--- workflow.py
+++ workflow.py
@@ -1,5 +1,10 @@
 import awkward as ak
 import numpy as np
+import uproot
+import pandas as pd
+import warnings
+import os
+
 
 from pocket_coffea.workflows.base import BaseProcessorABC
 from pocket_coffea.utils.configurator import Configurator
@@ -48,7 +53,8 @@ class VHccBaseProcessor(BaseProcessorABC):
         self.events["ll"] = get_dilepton(
             self.events.ElectronGood, self.events.MuonGood
         )
-
+        
+        
         self.events["JetGood"], self.jetGoodMask = jet_selection(
             self.events, "Jet", self.params, "LeptonGood"
         )
@@ -70,12 +76,114 @@ class VHccBaseProcessor(BaseProcessorABC):
         self.events["JetGood_Ht"] = ak.sum(abs(self.events.JetGood.pt), axis=1)
 
     def define_common_variables_after_presel(self, variation):
-
-        if self.proc_type=="ZLL":
-            self.events["dilep_deltaR"] = self.events.ll.deltaR
-
+        high_pt_mask = (self.events.ll.pt > 150)
+        low_pt_mask = (self.events.ll.pt >= 50) & (self.events.ll.pt < 150)
         self.events["dijet"] = get_dijet(self.events.JetGood)
-        self.events["dijet_deltaR"] = self.events.dijet.deltaR
+        
+        # Determine the maximum length
+        max_length = len(self.events.ll)
+        ll_low = self.events.ll[low_pt_mask]
+        ll_high = self.events.ll[high_pt_mask]
+        dijet_low = self.events.dijet[low_pt_mask]
+        dijet_high = self.events.dijet[high_pt_mask]
+        # Pad the arrays to match the maximum length
+        ll_low_padded = ak.pad_none(ll_low, max_length, axis=0)
+        ll_high_padded = ak.pad_none(ll_high, max_length, axis=0)
+        dijet_low_padded = ak.pad_none(dijet_low, max_length, axis=0)
+        dijet_high_padded = ak.pad_none(dijet_high, max_length, axis=0)
+
+        # Concatenate the jagged arrays along the first axis
+        self.events["ll_low"] = ll_low_padded
+        self.events["ll_high"] = ll_high_padded
+        self.events["dijet_low"] = dijet_low_padded
+        self.events["dijet_high"] = dijet_high_padded
+        
+        
+        
+        #self.events["dijet_pt"] = self.events.dijet.pt
+        
+        if self.proc_type=="ZLL":
+            ### Low_pt dilepton
+            self.events["dijet_deltaR_low"] = self.events.dijet_low.deltaR
+            self.events["dijet_deltaPhi_low"] = self.events.dijet_low.deltaPhi
+            self.events["dijet_deltaEta_low"] = self.events.dijet_low.deltaEta
+            self.events["dijet_m_low"] = self.events.dijet_low.mass
+            self.events["dijet_pt_low"] = self.events.dijet_low.pt
+            self.events["dilep_deltaR_low"] = self.events.ll_low.deltaR
+            self.events["dilep_pt_low"] = self.events.ll_low.pt
+            self.events["dilep_mass_low"] = self.events.ll_low.mass
+            self.events["dilep_phi_low"] = self.events.ll_low.deltaPhi
+            self.events["dilep_eta_low"] = self.events.ll_low.deltaEta
+            self.events["pt_ratio_low"] = self.events.ll_low.pt/self.events.dijet_low.pt
+            self.events["ZH_delphi_low"] = np.abs(self.events.ll_low.delta_phi(self.events.dijet_low))
+            self.angle21 = (abs(self.events.ll_low.l2phi - self.events.dijet_low.j1Phi) < np.pi)
+            self.angle22 = (abs(self.events.ll_low.l2phi - self.events.dijet_low.j2Phi) < np.pi)
+            self.events["deltaPhi_l2_j1_low"] = ak.where(self.angle21, abs(self.events.ll_low.l2phi - self.events.dijet_low.j1Phi), 2*np.pi - abs(self.events.ll_low.l2phi - self.events.dijet_low.j1Phi))              
+            self.events["deltaPhi_l2_j2_low"] = ak.where(self.angle22, abs(self.events.ll_low.l2phi - self.events.dijet_low.j2Phi), 2*np.pi - abs(self.events.ll_low.l2phi - self.events.dijet_low.j2Phi))
+            
+            ### High_pt dilepton
+            
+            self.events["dijet_deltaR_high"] = self.events.dijet_high.deltaR
+            self.events["dijet_deltaPhi_high"] = self.events.dijet_high.deltaPhi
+            self.events["dijet_deltaEta_high"] = self.events.dijet_high.deltaEta
+            self.events["dijet_m_high"] = self.events.dijet_high.mass
+            self.events["dijet_pt_high"] = self.events.dijet_high.pt
+            self.events["dilep_deltaR_high"] = self.events.ll_high.deltaR
+            self.events["dilep_pt_high"] = self.events.ll_high.pt
+            self.events["dilep_mass_high"] = self.events.ll_high.mass
+            self.events["dilep_phi_high"] = self.events.ll_high.deltaPhi
+            self.events["dilep_eta_high"] = self.events.ll_high.deltaEta
+            self.events["pt_ratio_high"] = self.events.ll_high.pt/self.events.dijet_high.pt
+            self.events["ZH_delphi_high"] = np.abs(self.events.ll_high.delta_phi(self.events.dijet_high))
+            self.angle21_h = (abs(self.events.ll_high.l2phi - self.events.dijet_high.j1Phi) < np.pi)
+            self.angle22_h = (abs(self.events.ll_high.l2phi - self.events.dijet_high.j2Phi) < np.pi)
+            self.events["deltaPhi_l2_j1_high"] = ak.where(self.angle21_h, abs(self.events.ll_high.l2phi - self.events.dijet_high.j1Phi), 2*np.pi - abs(self.events.ll_high.l2phi - self.events.dijet_high.j1Phi))
+            self.events["deltaPhi_l2_j2_high"] = ak.where(self.angle22_h, abs(self.events.ll_high.l2phi - self.events.dijet_high.j2Phi), 2*np.pi - abs(self.events.ll_high.l2phi - self.events.dijet_high.j2Phi))
+            
+            # Create a record for the low_ variables
+            # Create a record for the low_ variables
+            low_variables = ak.zip({
+                "dilep_deltaR": self.events["dilep_deltaR_low"],
+                "dilep_pt": self.events["dilep_pt_low"],
+                "dilep_mass": self.events["dilep_mass_low"],
+                "dilep_phi": self.events["dilep_phi_low"],
+                "dilep_eta": self.events["dilep_eta_low"],
+                "pt_ratio": self.events["pt_ratio_low"],
+                "ZH_delphi": self.events["ZH_delphi_low"],
+                "deltaPhi_l2_j1": self.events["deltaPhi_l2_j1_low"],
+                "deltaPhi_l2_j2": self.events["deltaPhi_l2_j2_low"],
+                "dijet_deltaR": self.events["dijet_deltaR_low"],
+                "dijet_deltaPhi": self.events["dijet_deltaPhi_low"],
+                "dijet_deltaEta": self.events["dijet_deltaEta_low"],
+                "dijet_m": self.events["dijet_m_low"],
+                "dijet_pt": self.events["dijet_pt_low"]
+            })
+
+            # Create a record for the high_ variables
+            high_variables = ak.zip({
+                "dilep_deltaR": self.events["dilep_deltaR_high"],
+                "dilep_pt": self.events["dilep_pt_high"],
+                "dilep_mass": self.events["dilep_mass_high"],
+                "dilep_phi": self.events["dilep_phi_high"],
+                "dilep_eta": self.events["dilep_eta_high"],
+                "pt_ratio": self.events["pt_ratio_high"],
+                "ZH_delphi": self.events["ZH_delphi_high"],
+                "deltaPhi_l2_j1": self.events["deltaPhi_l2_j1_high"],
+                "deltaPhi_l2_j2": self.events["deltaPhi_l2_j2_high"],
+                "dijet_deltaR": self.events["dijet_deltaR_high"],
+                "dijet_deltaPhi": self.events["dijet_deltaPhi_high"],
+                "dijet_deltaEta": self.events["dijet_deltaEta_high"],
+                "dijet_m": self.events["dijet_m_high"],
+                "dijet_pt": self.events["dijet_pt_high"]
+            })
+
+
+            # Add the records to the events
+            self.events["low_variables"] = low_variables
+            self.events["high_variables"] = high_variables
+
+        
+        
 
         if self.proc_type=="ZNuNu":
             self.events["deltaPhi_jet1_MET"] = np.abs(self.events.MET.delta_phi(self.events.JetGood[:,0]))
@@ -90,3 +198,20 @@ class VHccBaseProcessor(BaseProcessorABC):
         #print("CvsL sort CvsL:", self.events["JetsCvsL"][self.events["nJetGood"]>=3].btagDeepFlavCvL)
 
         self.events["dijet_csort"] = get_dijet(self.events.JetsCvsL)
+        
+        # Suppress FutureWarning
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", category=FutureWarning)
+            
+             # write to root files
+            # Check if the directory exists
+            if not os.path.exists(f"Saved_root_files/{self.events.metadata['dataset']}"):
+                # If not, create it
+                os.system(f"mkdir -p Saved_root_files/{self.events.metadata['dataset']}")
+            # Write the events to a ROOT file
+            # Write the events to a ROOT file
+            with uproot.recreate(f"Saved_root_files/{self.events.metadata['dataset']}/{self.events.metadata['filename'].split('/')[-1].replace('.root','')}_{int(self.events.metadata['entrystart'])}_{int(self.events.metadata['entrystop'])}.root") as f: 
+                f["low_variables"] = ak.to_pandas(low_variables)
+                f["high_variables"] = ak.to_pandas(high_variables) 
+            ak.to_pandas(self.events["low_variables"]).to_parquet(f"Saved_root_files/{self.events.metadata['dataset']}/{self.events.metadata['filename'].split('/')[-1].replace('.root','')}_{int(self.events.metadata['entrystart'])}_{int(self.events.metadata['entrystop'])}_low_vars.parquet")
+            ak.to_pandas(self.events["high_variables"]).to_parquet(f"Saved_root_files/{self.events.metadata['dataset']}/{self.events.metadata['filename'].split('/')[-1].replace('.root','')}_{int(self.events.metadata['entrystart'])}_{int(self.events.metadata['entrystop'])}_high_vars.parquet")  
